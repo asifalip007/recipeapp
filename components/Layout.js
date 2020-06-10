79bd0780adad8e70062e7a40637194096df1b1ba
asifalip007 <asifaliputhukkolly@gmail.com>
2020-06-10 23:42:25 +0530

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function Layout(props) {
+    return (
+        <div>
+            <Head>
+                <title>Recipe Book</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/css/uikit.min.css" />
+                <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/js/uikit.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/js/uikit-icons.min.js"></script>
+            </Head>
+            {props.children}
+        </div>
+    )
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+function Index() {
+  return (
+    <Layout>
+      <h1 className='uk-margin uk-padding-small uk-text-center'>RECIPE BOOK</h1>
+    </Layout>
+  )
+}
+
+export default Index;
